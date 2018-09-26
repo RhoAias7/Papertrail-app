5eac187ea0aa8b9a18ebf2ce082270b838100509
Ayo <ayofouad@gmail.com>
2018-09-26 13:18:53 +0000

    changed db4

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -2,7 +2,7 @@
 
 $dbServername = "eu-cdbr-west-02.cleardb.net";
 $dbUsername = "b8eaa38c885d5d";
-$dbPassword = "b902383a";
+$dbPassword = "b902383a1";
 $dbName = "b8eaa38c885d5d@eu-cdbr-west-02.cleardb.net";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
