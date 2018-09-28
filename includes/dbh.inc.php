953bdcb4992a966bc182e5b06d13b9107b58a684
Ayo <ayofouad@gmail.com>
2018-09-28 08:28:09 +0000

    heroku

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,8 +1,8 @@
 <?php
 
-$dbServername = "127.0.0.1";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "loginSystem";
+$dbServername = "b8eaa38c885d5d@eu-cdbr-west-02.cleardb.net";
+$dbUsername = "b8eaa38c885d5d";
+$dbPassword = "b902383a";
+$dbName = "heroku_b3ae3499c1695e2";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName); ?>
