e339a6eec04c24548722c604c40db3ac09318c0d
robrown97 <yourGitHub@email.com>
2018-11-14 15:04:37 +0000

    accepting coreect versions

--- Dashboard/index.php
+++ Dashboard/index.php
@@ -1,5 +1,4 @@
 <html lang="en">
-<<<<<<< HEAD
 
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
@@ -95,106 +94,9 @@
 
     <div class="container-fluid">
         <div class="row">
-=======
-   <head>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-      <title>Papertrail - Dashboard</title>
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-      <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
-      <script src="../stuff/js/lib/arbor.js"></script>
-      <script src="../stuff/js/lib/graphics.js"></script>
-      <script src="../stuff/js/lib/renderer.js"></script>
-      <!-- <script src="main.js"></script> -->
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
-      <link rel="stylesheet" href="indexStyle.css">
-   </head>
-   <body>
-      <nav class="navbar navbar-expand-md navbar-dark bg-primary">
-         <a class="navbar-brand" href="../landing.php">
-         <img src="log.png" width="50px" height="50px" alt="">
-         </a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
-         <span class="navbar-toggler-icon"></span>
-         </button>
-         <div class="navbar-collapse collapse" id="collapsingNavbar">
-            <ul class="navbar-nav ml-auto">
-               <!--
-                  <li class="nav-item active">
-                     <a class="nav-link" href="../landing.php" data-target="#myModal" data-toggle="modal">Home</a>
-                  </li>
-                  <li class="nav-item active">
-                     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Rating</button>               
-                  </li>
-                  -->
-            </ul>
-         </div>
-      </nav>
-      <!-- Button trigger modal -->
-      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch Side Hoe
-</button>
-
-              <!-- Modal -->
-              <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                     <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                   <div class="modal-header">
-                                          <h5 class="modal-title" id="exampleModalLabel">Side Hoe</h5>
-                                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                          <span aria-hidden="true">&times;</span>
-                                          </button>
-                                   </div>
-                                   <div class="modal-body">
-                                          <div class="container-fluid">
-                                                 <div class="row"><form method="post" action="">
-                                                        <div class="col-md-3">
-                                                                <label>Good!</label>
-                                                               <input type="radio" value="good" name="insert1">
-                                                        </div>
-                                                        <div class="col-md-3">
-                                                              <label>Meh :/</label>
-                                                               <input type="radio" value="meh" name="insert1">
-                                                        </div>
-                                                        <div class="col-md-3">
-                                                                <label>Terriable :(</label>
-                                                               <input type="radio"  value="bad" name="insert1">
-                                                        </div>
-                                                 </div>
-                                                 
-                                                        <div class="form-group">
-                                                               <label for="formGroupExampleInput">What do you think</label>
-                                                               <input type="text" class="form-control" name="comment"  placeholder="Comment">
-                                                        </div>
-                                                 
-
-                                                 <div class="row">
-                                                        <div class="col-md-3">
-                                                               <button type="button" class="btn btn-primary">Download</button>
-                                                        </div>
-                                                        <div class="col-md-3">
-                                                               <button type="button" class="btn btn-danger">Delete</button>
-                                                        </div>
-                                                 </div>
-
-                                          </div>
-                                   </div>
-                                   <div class="modal-footer">
-                                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                          <!--<button type="button" method = "post" name = "save" class="btn btn-primary">Save changes</button>
-                                            -->
-                                            <input type="submit" />
-                                   </div></form>
-                            </div>
-                     </div>
-              </div>
-      <div class="container-fluid">
-         <div class="row">
->>>>>>> ayo-branch
             <div class="">
                 <canvas id="viewport" width="1366" height="768"></canvas>
             </div>
-<<<<<<< HEAD
         </div>
         <div class="row">
             <div class="col-6">
@@ -271,62 +173,3 @@
 </script>
 
 </html>
-=======
-         </div>
-         <div class="row">
-            <div class="col-sm">
-               <button type="button" id="bottomLeft"  class="dashControl" data-toggle="modal" data-target="#bottomLeft1">
-                  <p><i class="fas fa-cloud-upload-alt"></i> </p>
-               </button>
-            </div>
-            <div class="col-sm" id="bottomCenter">
-            <?php include "upload.php"; ?>
-            <?php include "rate.php"; ?>
-            </div>
-            <div class="col-sm">
-               <button type="button" id="bottomRight" class="dashControl">
-                  <p><i class="fas fa-comments"></i></p>
-               </button>
-            </div>
-         </div>
-      </div>
-      <!--Model for upload-->
-      <!-- Modal -->
-      <div class="modal fade" id="bottomLeft1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-         <div class="modal-dialog uploadForm" role="document">
-            <div class="modal-content">
-               <div class="modal-header">
-                  <h5 class="fileTitle">Upload your file here</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                  </button>
-               </div>
-               <div class="modal-body uploadModal">
-                  <div class="modal-body" id="drop_zone">
-                     <i class="fas fa-cloud-upload-alt" id="formFileIcon"></i>                
-                     <form method="post" role="form" enctype="multipart/form-data">
-                        <input type="file" name="file" id="file"/>
-                  </div>
-                  <br>
-                  <input type="text" name="filename" class="form-control" id="fileNameUpload" placeholder="Filename..">
-                  <input type="text" name="shortDesc" class="form-control" id="fileCommentUpload" placeholder="Please enter a short description.">
-                  <input type="checkbox" name="vehicle" value="Agree">Allow us to share your Notes.<br>
-                  <button id="send" type="upload" name="upload" class="btn uploadBtn">Upload</button>
-                  </form>
-                  <p id="fileName"></p>
-               </div>
-            </div>
-         </div>
-      </div>
-      <!--Model for upload-->
-      <script>
-         var sys = arbor.ParticleSystem(5, 10, 0.1341, true, 55);
-         sys.parameters({gravity: true});
-         sys.renderer = Renderer("#viewport");
-         <?php
-            include "show.php";
-            ?>
-      </script>
-   </body>
-</html>
->>>>>>> ayo-branch
